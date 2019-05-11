b54382c54cbe93c1393434d65c10b417161592b9
manzano <raulmanzanoiglesias@gmail.com>
2019-05-11 15:36:11 +0200

    varios cambios

--- web/app/controllers/Secure.java
+++ web/app/controllers/Secure.java
@@ -18,14 +18,34 @@ public class Secure extends Controller {
     }
 
     public static void authenticate(String username, String password){
+    	// se incorpora un contador de intentos para deshabilitar los usuarios que hagan muchos intentos en una misma sesion.
         User u = User.loadUser(username);
+        
+        if (session.get("intentos")==null)
+        	{session.put("intentos","0");
+        	System.out.println("Estableciendo valor inicial a "+ session.get("intentos"));}
+        
         if (u != null && u.getPassword().equals(HashUtils.getMd5(password))){
-            session.put("username", username);
+        	//session.put("intentos",null);
+        	//System.out.println(session.get("intentos"));
+        	session.put("username", username);
             session.put("password", password);
             Application.index();
         }else{
-            flash.put("error", Messages.get("Public.login.error.credentials"));
-            login();
+            if ((session.get("intentos")!=null) && (Integer.valueOf(session.get("intentos")) <= 3))
+            {
+            	flash.put("error", Messages.get("Public.login.error.credentials"));
+            	session.put("intentos",String.valueOf(Integer.valueOf(session.get("intentos"))+1));
+            	System.out.println("Incrementando por errores a "+ session.get("intentos"));
+            	login();	
+            }
+            else
+            {	//usuario deshabilitado
+            	u.setPassword(null);
+            	flash.put("error", Messages.get("Public.login.error.disabled"));
+            	System.out.println("Excedido en "+ session.get("intentos"));
+            	login();
+            }
         }
 
     }
